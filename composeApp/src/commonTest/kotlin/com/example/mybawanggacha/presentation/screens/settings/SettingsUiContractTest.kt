4a57f2f9e196993fcef5f8c0c654c630e58580a8
Varasina Farmadani <sina@sinanonym.my.id>
2026-06-08 10:51:29 +0700

    test(settings): update settings ui test

--- composeApp/src/commonTest/kotlin/com/example/mybawanggacha/presentation/screens/settings/SettingsUiContractTest.kt
+++ composeApp/src/commonTest/kotlin/com/example/mybawanggacha/presentation/screens/settings/SettingsUiContractTest.kt
@@ -1,5 +1,6 @@
 package com.example.mybawanggacha.presentation.screens.settings
 
+import com.example.mybawanggacha.domain.settings.model.AiApiModel
 import com.example.mybawanggacha.domain.settings.model.AppColorScheme
 import com.example.mybawanggacha.domain.settings.model.NetworkMode
 import kotlin.test.Test
@@ -19,6 +20,14 @@ class SettingsUiContractTest {
     fun networkMode_allowsNetwork_shouldMatchMode() {
         assertTrue(NetworkMode.Auto.allowsNetwork)
         assertFalse(NetworkMode.OfflineOnly.allowsNetwork)
+    }
+
+    @Test
+    fun aiApiModel_fromString_shouldFallbackToGemini35Flash() {
+        assertEquals(AiApiModel.Gemini35Flash, AiApiModel.fromString(null))
+        assertEquals(AiApiModel.Gemini35Flash, AiApiModel.fromString("broken"))
+        assertEquals(AiApiModel.Gemini25Flash, AiApiModel.fromString("gemini-2.5-flash"))
+        assertEquals(AiApiModel.Gemini25Pro, AiApiModel.fromString("gemini25pro"))
     }
 
     @Test
